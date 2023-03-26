726cb69a8eae1d9ae3558449da40a57134cf566b
NassKillers <mr.nedjar.nassim@gmail.com>
2023-03-26 13:18:15 +0200

    add fa angle up

--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@ import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faFolder } from '@fortawesome/free-regular-svg-icons'
-import { faEnvelope, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
+import { faAngleUp, faEnvelope, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 import { faSquareGithub, faLinkedin, faHtml5, faCss3Alt, faSass, faJs, faVuejs, faPhp } from '@fortawesome/free-brands-svg-icons'
 
-library.add(faFolder, faEnvelope, faArrowUpRightFromSquare, faSquareGithub, faLinkedin, faHtml5, faCss3Alt, faSass, faJs, faVuejs, faPhp)
+library.add(faAngleUp, faFolder, faEnvelope, faArrowUpRightFromSquare, faSquareGithub, faLinkedin, faHtml5, faCss3Alt, faSass, faJs, faVuejs, faPhp)
 
 createApp(App).component('fa', FontAwesomeIcon).use(router).mount('#app')
