0bfd5c422f5b24a52c35d62c833b23d50fbe78d4
NassKillers <mr.nedjar.nassim@gmail.com>
2023-04-24 01:08:47 +0200

    add icon angle up

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import App from './App.vue'
 import router from './router'
 
 import { OhVueIcon, addIcons } from "oh-vue-icons";
-import { FaHtml5, FaCss3Alt, FaSass, SiJavascript, FaVuejs, FaPhp, SiFirebase, SiExpress, SiMongodb, FaNode, FaRegularFolder, SiGithub, FaLinkedin, FaEnvelope, HiExternalLink } from "oh-vue-icons/icons";
+import { FaHtml5, FaCss3Alt, FaSass, SiJavascript, FaVuejs, FaPhp, SiFirebase, SiExpress, SiMongodb, FaNode, FaRegularFolder, SiGithub, FaLinkedin, FaEnvelope, HiExternalLink, FaAngleUp } from "oh-vue-icons/icons";
 
-addIcons(FaHtml5, FaCss3Alt, FaSass, SiJavascript, FaVuejs, FaPhp, SiFirebase, SiExpress, SiMongodb, FaNode, FaRegularFolder, SiGithub, FaLinkedin, FaEnvelope, HiExternalLink );
+addIcons(FaHtml5, FaCss3Alt, FaSass, SiJavascript, FaVuejs, FaPhp, SiFirebase, SiExpress, SiMongodb, FaNode, FaRegularFolder, SiGithub, FaLinkedin, FaEnvelope, HiExternalLink, FaAngleUp );
 
 createApp(App).component("fa", OhVueIcon).use(router).mount('#app')
